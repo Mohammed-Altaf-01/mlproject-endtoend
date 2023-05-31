d6fcff69eb1d92cc6f8c6951ea49c6e0be8b340b
Mohammed_Altaf <mohammedaltaf4316@gmail.com>
2023-05-31 13:32:30 +0530

    added code to data ingestion and tested it

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 
 # custom imports
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
 from src.logger import logging
 
 @dataclass
@@ -48,7 +49,10 @@ class DataIngestion:
 
 if __name__ =="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train,test =obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train,test)
 
         
 
